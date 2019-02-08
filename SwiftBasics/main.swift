d168d8edf2da0745b21ff56c8b98419e3faef1c8
Zach Swartz <zachswartz@Zachs-MacBook-Pro.local>
2019-02-07 23:21:05 -0600

    Initial Commit

---
+++ SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Zach Swartz on 2/7/19.
+//  Copyright © 2019 Zach Swartz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
